717279387f5216c7d55ffc7f4b8f53267f521f6e
waribiki95 <77254241+waribiki95@users.noreply.github.com>
2022-04-18 15:50:59 +0700

    up form dang ky sinh vien

--- Asp.netMVC/WebDemo2/WebDemo2/Models/Student.cs
+++ Asp.netMVC/WebDemo2/WebDemo2/Models/Student.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
 
 namespace WebDemo2.Models
 {
     public class Student
     {
-        
+        [DisplayName("Mã sinh viên")]
         public String Id { get; set; }
+        [DisplayName("Mật khẩu")]
+        public string Password { get; set; }
+        [DisplayName("Họ tên")]
+        public string FullName { get; set; }
+        [DisplayName("Giới tính")]
+        public bool Gender { get; set; }
+        [DisplayName("Ngày sinh")]
+        public DateTime Birthday { get; set; }
+        [DisplayName("Ghi chú")]
+        public string Notes { get; set; }
     }
 }
