9493861e7bee2e4fe0108e0f28746fdc96ca0e5b
waribiki95 <77254241+waribiki95@users.noreply.github.com>
2022-04-28 18:47:28 +0700

    controllers and models

---
+++ Asp.netMVC/WebDemo2/WebDemo2/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDemo2.Models;
+
+namespace WebDemo2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detail(User user)
+        {
+            return View(user);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            return View("Detail",user);
+        }
+    }
+}
